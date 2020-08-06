3282433c18922d913eb905daf6f27b3752023239
Jordan Taylor <jdtaylor7@gmail.com>
2020-08-06 15:11:39 -0700

    Add convenience function for texture scaling.

--- include/shapes.hpp
+++ include/shapes.hpp
@@ -14,6 +14,23 @@ const std::vector<unsigned int> square_indices = {
     1, 2, 3,  // left triangle
 };
 
+std::vector<float> scale_square_texture_coords(float scale_factor)
+{
+    std::size_t texture_coords_offset = 6;
+    std::size_t bytes_per_vertex = 8;
+    std::size_t num_texture_coords = 2;
+
+    std::vector<float> scaled_vertices = square_vertices;
+
+    for (std::size_t i = texture_coords_offset; i < scaled_vertices.size(); i += bytes_per_vertex)
+    {
+        for (std::size_t j = i; j < i + num_texture_coords; j++)
+            scaled_vertices[j] *= scale_factor;
+    }
+
+    return scaled_vertices;
+}
+
 const std::vector<float> cube_vertices = {
     // positions          // normals           // texture coords
     -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f,  0.0f,
